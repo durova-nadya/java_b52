e3211efd0c6a3ee958bc7c21f5062aed376890a3
durova-nadya <joyhope@yandex.ru>
2025-03-09 15:23:17 +0300

    Добавлен базовый класс

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -1,32 +1,7 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupRemovalTests {
-    private WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1054, 808));
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.findElement(By.linkText("Logout")).click();
-        driver.quit();
-    }
+public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
@@ -47,15 +22,5 @@ public class GroupRemovalTests {
         driver.findElement(By.name("selected[]")).click();
         driver.findElement(By.name("delete")).click();
         driver.findElement(By.linkText("group page")).click();
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-
     }
 }
---
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+    protected static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(1536, 824));
+            driver.findElement(By.name("user")).click();
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).click();
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+
+    }
+}
