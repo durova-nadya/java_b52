569f98abc7e0855088ade68b946db0ef38deeb33
durova-nadya <joyhope@yandex.ru>
2025-03-09 20:39:23 +0300

    Классы разложены по пакетам

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,3 +1,5 @@
+package tests;
+
 import model.GroupData;
 import org.junit.jupiter.api.Test;
 
@@ -6,21 +8,21 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void canCreateGroup() {
-        openGroupPage();
-        createGroup(new GroupData("Friends", "Best friends", "comment"));
+        app.openGroupPage();
+        app.createGroup(new GroupData("Friends", "Best friends", "comment"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        openGroupPage();
-        createGroup(new GroupData());
+        app.openGroupPage();
+        app.createGroup(new GroupData());
     }
 
     @Test
     public void canCreateGroupWithNameOnly() {
-        openGroupPage();
+        app.openGroupPage();
         var emptyGroup = new GroupData();
         var groupWithName = emptyGroup.withName("some name");
-        createGroup(groupWithName);
+        app.createGroup(groupWithName);
     }
 }
