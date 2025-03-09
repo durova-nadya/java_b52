9250106e77dbb411cebb53c0d13e37b6d312e630
durova-nadya <joyhope@yandex.ru>
2025-03-09 18:05:09 +0300

    Добавлен модельный класс для группы

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -6,12 +7,20 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void canCreateGroup() {
         openGroupPage();
-        createGroup("Friends", "Best friends", "comment");
+        createGroup(new GroupData("Friends", "Best friends", "comment"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
     }
 }
--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupRemovalTests extends TestBase {
@@ -6,7 +7,7 @@ public class GroupRemovalTests extends TestBase {
     public void canRemoveGroup() {
         openGroupPage();
         if (!isGroupPresent()) {
-            createGroup("Friends", "Best friends", "comment");
+            createGroup(new GroupData("Friends", "Best friends", "comment"));
         }
         revomeGroup();
     }
