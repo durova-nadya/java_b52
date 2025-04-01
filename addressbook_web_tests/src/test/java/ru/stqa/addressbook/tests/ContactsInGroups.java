74075a024026f6a24c83a196104aea66efe0463b
durova-nadya <joyhope@yandex.ru>
2025-04-01 21:35:08 +0300

    Переделан алгоритм добавления контакта в группу

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactsInGroups.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactsInGroups.java
@@ -7,6 +7,7 @@ import ru.stqa.addressbook.model.GroupData;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 public class ContactsInGroups extends TestBase{
@@ -31,6 +32,12 @@ public class ContactsInGroups extends TestBase{
 
     @Test
     void canAddContactInGroup() {
+
+        boolean isGroup = false;
+        List<ContactData> oldRelated = new ArrayList<>();
+        var contactCheck = new ContactData();
+        var groupCheck = new GroupData();
+
         if (app.hbm().getContactCount() == 0) {
             app.hbm().createContact(new ContactData("", "Саша", "Наш", "Санкт-Петербург", "pushkin.as@mail.ru"));
         }
@@ -38,32 +45,45 @@ public class ContactsInGroups extends TestBase{
             app.hbm().createGroup(new GroupData("", "Friends", "Best friends", "comment"));
         }
 
-        var allContacts = app.hbm().getContactList();;
-        var rnd = new Random();
-        var indexContact = rnd.nextInt(allContacts.size());
-        var contact = app.hbm().getContactList().get(indexContact);
-
         var allGroups = app.hbm().getGroupList();
-        var indexGroup = rnd.nextInt(allGroups.size());
-        var group = app.hbm().getGroupList().get(indexGroup);
+        var allContacts = app.hbm().getContactList();
 
-        var oldRelated = app.hbm().getContactsInGroup(group);
-        app.contacts().addContactInGroup(contact, group);
-        var newRelated = app.hbm().getContactsInGroup(group);
+        for (var group : allGroups) {
+            oldRelated = app.hbm().getContactsInGroup(group);
+            for (var contact : allContacts) {
+                if (!app.contacts().contactInGroup(contact.id(), oldRelated)) {
+                    app.contacts().addContactInGroup(contact, group);
+                    isGroup = true;
+                    groupCheck = group;
+                    contactCheck = contact;
+                    break;
+                }
+            }
+            if (isGroup) {break;}
+        }
+        if (! isGroup) {
+            groupCheck = app.hbm().getGroupList().get(0);
+            oldRelated = app.hbm().getContactsInGroup(groupCheck);
+            app.hbm().createContact(new ContactData("", "Саша", "Наш", "Санкт-Петербург", "pushkin.as@mail.ru"));
+            int maxId = (int) app.hbm().getContactCount();
+            contactCheck = app.hbm().getContactList().get(maxId - 1);
+            app.contacts().addContactInGroup(contactCheck, groupCheck);
+        }
+
+        var newRelated = app.hbm().getContactsInGroup(groupCheck);
         Comparator<ContactData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newRelated.sort(compareById);
         var expectedList = new ArrayList<>(oldRelated);
-        if (!app.contacts().contactInGroup(contact.id(), oldRelated)) {
-            expectedList.add(contact.withId(contact.id()));
-        }
+        expectedList.add(contactCheck.withId(contactCheck.id()));
         expectedList.sort(compareById);
+
         Assertions.assertEquals(expectedList, newRelated);
     }
 
     @Test
-    void canDeleteContactFromGroup() {
+    void canRemoveContactFromGroup() {
         if (app.hbm().getGroupCount() == 0) {
             app.hbm().createGroup(new GroupData("", "Friends", "Best friends", "comment"));
         }
