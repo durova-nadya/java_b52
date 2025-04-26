7e8a5430cedbd668327c8f5cceda9f49b1d7ad26
durova-nadya <joyhope@yandex.ru>
2025-04-26 14:12:35 +0300

    Добавлен HttpHelper

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -18,6 +18,8 @@ public class ApplicationManager {
 
     private SessionHelper sessionHelper;
 
+    private HttpSessionHelper httpSessionHelper;
+
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -49,5 +51,16 @@ public class ApplicationManager {
             sessionHelper = new SessionHelper(this);
         }
         return sessionHelper;
+    }
+
+    public HttpSessionHelper http() {
+        if (httpSessionHelper == null) {
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
     }
 }
